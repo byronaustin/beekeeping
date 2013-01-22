64f5cc38d21786219f596a7852e3cc186c2d4e57
Byron Austin <byronaustin007@gmail.com>
2013-01-22 13:44:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beekeeping::Application.config.secret_token = '2271567ad5e6c3e9f8b1c43ff2b55632388f264961be037c78e19b1aa7b2c5f7cdc2f5195d6fd5d267d1a373cefe4db21f5641dfe6cacbc4f95a36330a6096c0'
